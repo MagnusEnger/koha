c82df1e572bb389dc1d90d90688978aed9834f23
Colin Campbell <colin.campbell@ptfs-europe.com>
2014-03-26 16:21:58 +0000

    Add basic methods to EDI::Message
    Basic Create Read Update Delete defined
    Will need methods to allow updating the foreign key fields
    by data

--- Koha/EDI/Message.pm
+++ Koha/EDI/Message.pm
@@ -40,6 +40,8 @@ sub get_all {
     edifact_messages.date_sent, edifact_messages.vendor_id,
     edifact_messages.status, edifact_messages.basketno,
     edifact_messages.invoiceid,
+    edifact_messages.edi,
+    edifact_messages.remote_file,
     aqinvoices.invoicenumber,
     aqbooksellers.name as providername
     from edifact_messages 
@@ -49,4 +51,117 @@ sub get_all {
 ENDMSGSQL
     return $dbh->selectall_arrayref( $sql, { Slice => {} } );
 }
+
+sub retrieve {
+    my $self = shift;
+    if ( $self->{id} ) {
+        my $sql = <<'ENDSELSQL';
+    select edifact_messages.message_type,
+    edifact_messages.date_sent, edifact_messages.vendor_id,
+    edifact_messages.status, edifact_messages.basketno,
+    edifact_messages.invoiceid,
+    edifact_messages.edi,
+    edifact_messages.remote_file,
+    aqinvoices.invoicenumber,
+    aqbooksellers.name as providername
+    from edifact_messages 
+    left outer join aqbooksellers on edifact_messages.vendor_id = aqbooksellers.id
+    left outer join aqinvoices on edifact_messages.invoiceid = aqinvoices.invoiceid
+    where edifact_messages.id = ?
+ENDSELSQL
+        my $dbh = C4::Context->dbh;
+        my $arr_ref =
+          $dbh->selectall_arrayref( $sql, { Slice => {} }, $self->{id} );
+        if ( @{$arr_ref} ) {
+            for my $attribute ( keys %{ $arr_ref->[0] } ) {
+                $self->{$attribute} = $arr_ref->[0]->{$attribute};
+            }
+            return 1;    # return success
+        }
+    }
+    return;
+}
+
+sub insert {
+    my ($self) = @_;
+    my $sql = <<'ENDINSSQL';
+insert into edifact_messages 
+(message_type, date_sent, vendor_id, status, basketno, invoiceid, edi, remote_file)
+values (?,?,?,?,?,?,?,?)
+ENDINSSQL
+    my $dbh = C4::Context->dbh;
+    $dbh->do(
+        $sql, {}, $self->{message_type},
+        $self->{date_sent}, $self->{vendor_id}, $self->{status},
+        $self->{basketno},  $self->{invoiceid}, $self->{edi},
+        $self->{remote_file}
+    );
+    $self->{id} = $dbh->{mysql_insertid};
+    return;
+}
+
+sub del {
+    my $self = shift;
+    if ( $self->{id} ) {
+        my $dbh = C4::Context->dbh;
+        return $dbh->do( 'delete from edifact_messages where id = ?',
+            {}, $self->{id} );
+    }
+    return;
+}
+
+sub update {
+    my ( $self, $arg_ref ) = @_;
+    if ( $self->{id} ) {
+        if ( exists $arg_ref->{message_type} ) {
+            $self->{message_type} = $arg_ref->{message_type};
+        }
+        if ( exists $arg_ref->{edi} ) {
+            $self->{edi} = $arg_ref->{edi};
+        }
+        if ( exists $arg_ref->{date_sent} ) {
+            $self->{date_sent} = $arg_ref->{date_sent};
+        }
+        if ( exists $arg_ref->{vendor_id} ) {
+            $self->{vendor_id} = $arg_ref->{vendor_id};
+        }
+        if ( exists $arg_ref->{status} ) {
+            $self->{status} = $arg_ref->{status};
+        }
+        if ( exists $arg_ref->{basketno} ) {
+            $self->{basketno} = $arg_ref->{basketno};
+        }
+        if ( exists $arg_ref->{invoiceid} ) {
+            $self->{invoiceid} = $arg_ref->{invoiceid};
+        }
+        if ( exists $arg_ref->{remote_file} ) {
+            $self->{remote_file} = $arg_ref->{remote_file};
+        }
+    }
+    return;
+}
+
+sub save {
+    my $self = shift;
+    if ( $self->{id} ) {
+        my $sql = <<'ENDUPDSQL';
+update edifact_messages
+set message_type = ?, date_sent = ?, vendor_id = ?, status = ?,
+basketno = ? invoiceid = ?, edi = ?, remote_file = ?
+where id = ?
+ENDUPDSQL
+        my $dbh = C4::Context->dbh;
+        return $dbh->do(
+            $sql, {},
+            $self->{message_type}, $self->{date_sent},
+            $self->{vendor_id},    $self->{status},
+            $self->{basketno},     $self->{invoiceid},
+            $self->{edi},          $self->{remote_file},
+            $self->{id}
+        );
+
+    }
+    return;
+}
+
 1;
--- t/EDI_message.t
+++ t/EDI_message.t
@@ -3,7 +3,7 @@
 use strict;
 use warnings;
 
-use Test::More tests => 2;
+use Test::More tests => 6;
 
 BEGIN {
     use_ok('Koha::EDI::Message');
@@ -15,4 +15,12 @@ my $obj = $class->new({});
 
 isa_ok($obj,$class);
 
+my $msg =  $class->new( { id => 1, edi => 'message contents' });
 
+is($msg->{id}, 1, "Id retrieval ok");
+is($msg->{edi}, 'message contents', "Contents retrieval ok");
+
+$msg->update({ id => 5, edi => 'different contents'});
+
+is($msg->{id}, 1, "Id protected during update");
+is($msg->{edi}, 'different contents', "Contents updated by update");
