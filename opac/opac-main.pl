f085edbe18b5af54f4d5bb88eea30d3f43276242
Martin Renvoize <martin.renvoize@ptfs-europe.com>
2014-04-02 08:12:54 +0000

    BUG8446, Follow up: Correct paths after file rename

--- opac/opac-main.pl
+++ opac/opac-main.pl
@@ -20,7 +20,7 @@ use strict;
 use warnings;
 use CGI;
 use C4::Auth;    # get_template_and_user
-use C4::Auth_with_Shibboleth qw( login_shib_url );
+use C4::Auth_with_shibboleth qw( login_shib_url );
 use C4::Output;
 use C4::NewsChannels;    # get_opac_news
 use C4::Languages qw(getTranslatedLanguages accept_language);
@@ -47,7 +47,7 @@ $template->param(
 if ( C4::Context->config('useshibboleth') ) {
     $template->param(
         shibbolethAuthentication => C4::Context->config('useshibboleth'),
-        shibbolethLoginUrl => C4::Auth_with_Shibboleth::login_shib_url($input)
+        shibbolethLoginUrl => C4::Auth_with_shibboleth::login_shib_url($input)
     );
 };
 
