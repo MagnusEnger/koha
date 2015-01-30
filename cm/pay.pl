b8ad51026a65cb95b59472fd71b4681c88155af6
Colin Campbell <colin.campbell@ptfs-europe.com>
2015-01-30 09:17:01 +0000

    pass data in pay.pl

--- cm/pay.pl
+++ cm/pay.pl
@@ -26,7 +26,7 @@ use C4::Context;
 use Koha::Database;
 use C4::Members qw( GetMember );
 use C4::Branch qw( GetBranchName );
-use Koha::Till
+use Koha::Till;
 
 my $q = CGI->new();
 my ( $template, $loggedinuser, $cookie, $user_flags ) = get_template_and_user(
@@ -39,12 +39,24 @@ my ( $template, $loggedinuser, $cookie, $user_flags ) = get_template_and_user(
     }
 );
 
+my $tillid = SessionTillId();
+my $schema = Koha::Database->new()->schema();
+
+my @payment_types = $schema->resultset('PaymentType')->search( { category => 'PaymentType', });
+
+my @transcodes = $schema->resultset('CashTranscode')->search( undef, { order_by => { -asc => 'code', }});
+
+
+
+
 ## Placeholder we need a screen for accepting payments not borrower related
 ## e.g. sale of goods
 ### and recording such as a transaction
 
 $template->param(
     branchname => $branchname,
+    paymenttypes => \@paymenttypes,
+    transcodes   => \@transcodes,
 );
 
 output_html_with_http_headers( $q, $cookie, $template->output );
