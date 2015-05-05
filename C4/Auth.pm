3f3c906ce8daabb37e1c26f5c5548b3d8f319043
David Roberts <david.roberts@ptfs-europe.com>
2015-05-05 13:38:36 +0000

    Put staff client password strength meter under syspref control

--- C4/Auth.pm
+++ C4/Auth.pm
@@ -412,6 +412,8 @@ sub get_template_and_user {
             intranetuserjs                                                             => C4::Context->preference("intranetuserjs"),
             intranetbookbag                                                            => C4::Context->preference("intranetbookbag"),
             suggestion                                                                 => C4::Context->preference("suggestion"),
+            PasswordStrength
+       => C4::Context->preference("PasswordStrength"),
             virtualshelves                                                             => C4::Context->preference("virtualshelves"),
             StaffSerialIssueDisplayCount                                               => C4::Context->preference("StaffSerialIssueDisplayCount"),
             EasyAnalyticalRecords                                                      => C4::Context->preference('EasyAnalyticalRecords'),
