628caac622e0b53abf338921e79ffd9cc8120e6a
Martin Renvoize <martin.renvoize@ptfs-europe.com>
2015-06-26 07:51:59 +0000

    Online Payments: SQL Update

--- installer/data/mysql/atomicupdate/online_payments.sql
+++ installer/data/mysql/atomicupdate/online_payments.sql
@@ -10,6 +10,7 @@ create trigger ac_transactions_insert before insert on `ac_transactions` for eac
 create table ac_transaction_accounts (
     accountline_id integer(11) not null,
     transaction_id integer(11) not null,
+    status tinyint(1) not null,
     foreign key (accountline_id) references accountlines (accountlines_id),
     foreign key (transaction_id) references ac_transactions (transaction_id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
